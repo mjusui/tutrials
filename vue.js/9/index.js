d0faa41cd5e9559e3fc96fcd2047330cc464cf35
mjusui <mjusui@gmail.com>
2019-01-19 04:50:13 +0000

    9

--- vue.js/9/index.js
+++ vue.js/9/index.js
@@ -1,8 +1,25 @@
 
+Vue.component('good-thing',{
+  data: function(){
+    return { count: 0, };
+  },
+  props:[ 'good', 'thing', ],
+  template: `
+    <div>
+      <input :value="thing" @input="$emit('input', $event.target.value)">
+      <div @click="count++;$emit('more-good')">count: {{ count }}, good: {{ good }}, thing: {{ thing }}</div>
+      <slot></slot>
+    </div>
+  `,
+});
+
+
 const inst = new Vue({
   el: '#instance',
   data: {
-    text: '',
-  },
+    good: 22,
+    thing: 33,
+    text: 'Good Thing',
+  }
 });
 
